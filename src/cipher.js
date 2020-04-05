a6cb037ac8b6ec21f15e6687381a7ac6186899bc
Adelia <adeliacristinecs@gmail.com>
2020-04-05 15:54:04 -0300

    commite test1

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,22 @@
 const cipher = {
-  // ...
-};
+  encode: function(offset, string) {
+   let resultadoCipher="";
+   for (let i = 0; i<msgentrada.length; i++){
+    let offset = document.getElementById("offset").value;
+    let  string = document.getElementById("msgentrada").value.toUpperCase();
+    let n = string.charCodeAt(i);
+    let posicaoDaLetra = (n - 65);
+    let posicaoDaLetraDeslocamento = ((posicaoDaLetra + offset) % 26);
+    let posicaoFinal = (posicaoDaLetraDeslocamento+65);
+    let letraFinal = String.fromCharCode(posicaoFinal);
+    resultadoCipher +=letraFinal;  
+   }
+   
+  return resultadoCipher;
+           
+  
+
+},
+}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+document.getElementById("encode").addEventListener("click", encode );
+document.getElementById("decode").addEventListener("click", decode );
+document.getElementById("limpar").addEventListener("click", limpar );
+//document.getElementById("sair").addEventListener("click", sair );
+//document.getElementById("Confirmar").addEventListener("click", confirmar );
+
+function encode(event){
+    event.preventDefault();
+    let offset = document.getElementById("offset").value;
+    let string = document.getElementById("msgentrada").value.toUpperCase();
+    let x= cipher.encode(offset,string);
+    document.getElementById("msgsaida").value= x;
+}
+function decode(event){
+    event.preventDefault();
+   let offset = document.getElementById("offset").value;
+   let string = document.getElementById("msgentrada").value.toUpperCase();
+   let x= cipher.decode(offset, string);
+   document.getElementById("msgsaida").value= x;
+}
+function limpar(event){
+    event.preventDefault();
+    document.getElementById("msgentrada").value="";
+    document.getElementById("msgsaida").value="";
+    console.log("limpou");
+}
+//function sair(event){
+  //  event.preventDefault();
+  //  window.close();   
+
+//function confirmar(event){
+  //  event.preventDefault();
+    //console.log("mudarhtml");
+    //window.location.href='gabi.html';
+    //document,getElementById("suaMsg").innerHTML= oi
+
