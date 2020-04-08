22676266c3bd5ab126e01ff6563050a178d1b7f2
Adelia <adeliacristinecs@gmail.com>
2020-04-08 15:45:14 -0300

    para atualização

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,53 @@
 const cipher = {
   encode: function(offset, string) {
    let resultadoCipher="";
-   for (let i = 0; i<msgentrada.length; i++){
-    let offset = document.getElementById("offset").value;
-    let  string = document.getElementById("msgentrada").value.toUpperCase();
+   for (let i = 0; i<string.length; i++){
     let n = string.charCodeAt(i);
-    let posicaoDaLetra = (n - 65);
-    let posicaoDaLetraDeslocamento = ((posicaoDaLetra + offset) % 26);
-    let posicaoFinal = (posicaoDaLetraDeslocamento+65);
-    let letraFinal = String.fromCharCode(posicaoFinal);
-    resultadoCipher +=letraFinal;  
-   }
-   
-  return resultadoCipher;
-           
-  
+     
+   //let offset = Number(document.getElementById("offset")).value;
+    //let  string = document.getElementById("msgentrada").value;
+    //let n = string.charCodeAt(i);
+   // let letraFinal = String.fromCharCode((((n-65) + offset) % 26)+65);
+    //resultadoCipher +=letraFinal;
+    //for (let i = 0; i<string.length; i++){
+      if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=99){
+    
+        let n = string.charCodeAt(i);
+        resultadoCipher += String.fromCharCode((((n-65) + offset) % 26)+65);
+    
+      } else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
+    
+     // let n = string.charCodeAt(i);
+        resultadoCipher += String.fromCharCode((((n-97) + offset) % 26)+97);
+    
+      } else if (string.charCodeAt(i)===32) {
+     // let n = string.charCodeAt(0);
+        resultadoCipher += String.fromCharCode(n);
+      }  else {
+    //let n = string.charCodeAt(0);
+        resultadoCipher+= String.fromCharCod(n);
+      }
+    return resultadoCipher;
+  }
+},
 
+
+
+   
+  //return resultadoCipher;
+      
+decode: function(offset, string) {
+  let resultadoCipher="";
+  for (let i = 0; i<string.length; i++){
+    
+    let n = string.charCodeAt(i);
+    let letraFinal = String.fromCharCode((((n-90) - offset) % 26)+90);
+    resultadoCipher +=letraFinal;
+  
+ 
+  }
+  
+ return resultadoCipher;
 },
 }
 
--- src/index.js
+++ src/index.js
@@ -1,39 +1,72 @@
 import cipher from './cipher.js';
 
-//console.log(cipher);
+
 document.getElementById("encode").addEventListener("click", encode );
 document.getElementById("decode").addEventListener("click", decode );
 document.getElementById("limpar").addEventListener("click", limpar );
-//document.getElementById("sair").addEventListener("click", sair );
-//document.getElementById("Confirmar").addEventListener("click", confirmar );
+document.getElementById("confirmar").addEventListener("click", confirmar );
 
 function encode(event){
-    event.preventDefault();
-    let offset = document.getElementById("offset").value;
-    let string = document.getElementById("msgentrada").value.toUpperCase();
-    let x= cipher.encode(offset,string);
-    document.getElementById("msgsaida").value= x;
-}
+  event.preventDefault();
+    let offset = parseInt(document.getElementById("offset").value);
+    let string = document.getElementById("msgentrada").value;//toUpperCase();
+    document.getElementById("msgsaida").innerHTML=cipher.encode(offset, string);
+    document.getElementById("confirmar").style.display="block";
+ }
 function decode(event){
-    event.preventDefault();
-   let offset = document.getElementById("offset").value;
-   let string = document.getElementById("msgentrada").value.toUpperCase();
-   let x= cipher.decode(offset, string);
-   document.getElementById("msgsaida").value= x;
-}
+  event.preventDefault();
+    let offset = parseInt(document.getElementById("offset").value);
+    let string = document.getElementById("msgentrada").value.toUpperCase();
+    document.getElementById("msgsaida").innerHTML=cipher.decode(offset, string);
+    document.getElementById("confirmar").style.display="block";
+ }
+
 function limpar(event){
     event.preventDefault();
+    document.getElementById("offset").value="";
     document.getElementById("msgentrada").value="";
     document.getElementById("msgsaida").value="";
-    console.log("limpou");
+    document.getElementById("pessoasCadastradas").value="";
 }
-//function sair(event){
-  //  event.preventDefault();
-  //  window.close();   
 
-//function confirmar(event){
-  //  event.preventDefault();
-    //console.log("mudarhtml");
-    //window.location.href='gabi.html';
-    //document,getElementById("suaMsg").innerHTML= oi
 
+//function sair(){
+   // event.preventDefault();
+  //  window.close();
+//
+function confirmar(event){
+    event.preventDefault();
+    document.getElementById("formulario").style.display="none";
+    document.getElementById("voltar").style.display="block";
+    document.getElementById("final").style.display="block";
+    document.getElementById("confirmar").style.display="none";
+  let pessoas = document.getElementById("pessoasCadastradas").value;
+  let gabi = document.getElementById("1").value;
+  let jardel = document.getElementById("2").value;
+  let offset = parseInt(document.getElementById("offset").value);
+  let string = document.getElementById("msgentrada").value.toUpperCase();
+  document.getElementById("voltar").addEventListener("click",voltar);
+ 
+   if (pessoas==gabi){
+     document.getElementById("suamsg").innerHTML= "Olá Gabi, voĉe recebeu uma nova messagem. Copie sua senha e sua mensagem e volte para decifra-lá."     
+     document.getElementById("off").innerHTML= "Sua senha é " + offset
+     document.getElementById("cifrada").innerHTML= "Sua mensagem é:" + cipher.encode(offset, string);
+     document.getElementById("decifrada").innerHTML= "Sua mensagem é:" + cipher.decode(offset, string);
+    }else if (pessoas==jardel){
+      document.getElementById("suamsg").innerHTML= "Olá Jardel, voĉe recebeu uma nova messagem. Copie sua senha e sua mensagem e volte para decifra-lá."
+      document.getElementById("off").innerHTML= "Sua senha é " + offset
+      document.getElementById("cifrada").innerHTML= "Sua mensagem é:" + cipher.encode(offset, string);
+      document.getElementById("decifrada").innerHTML= "Sua mensagem é:" + cipher.decode(offset, string);
+    }else {
+    document.getElementById("suamsg").innerHTML= "Olá Carla, voĉe recebeu uma nova messagem. Copie sua senha e sua mensagem e volte para decifra-lá."
+    document.getElementById("off").innerHTML= "Sua senha é " + offset
+    document.getElementById("cifrada").innerHTML= "Sua mensagem é:" + cipher.encode(offset, string);
+    document.getElementById("decifrada").innerHTML= "Sua mensagem é:" + cipher.decode(offset, string);
+    }
+  function voltar(){
+    document.getElementById("formulario").style.display="block";
+    document.getElementById("final").style.display="none";
+  }
+}
+
+   console.log();
