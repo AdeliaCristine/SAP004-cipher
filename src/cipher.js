c75572342f2ac73a2bf0f41a84f0debc499a041a
Adelia <adeliacristinecs@gmail.com>
2020-04-11 13:34:32 -0300

    projeto final

--- src/cipher.js
+++ src/cipher.js
@@ -13,13 +13,9 @@ const cipher = {
       } else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
         resultadoCipher += String.fromCharCode((((n-97) + offset) % 26)+97);
       } else if (string.charCodeAt(i)>=32 && string.charCodeAt(i)<=63){
-      //(string.charCodeAt(i)===32) {
-        //resultadoCipher += String.fromCharCode(n);
+      
         resultadoCipher += String.fromCharCode(((n-32) % 31)+32);
-      //} else if (string.charCodeAt(i)===128 ){
-        //resultadoCipher += String.fromCharCode(128);
-      //} else if (string.charCodeAt(i)===135 ){
-        //resultadoCipher += String.fromCharCode(135);
+     
       }else {
         resultadoCipher+= String.fromCharCode(n);
       }
@@ -28,28 +24,21 @@ const cipher = {
 },
       
 decode: function(offset, string) {
- // if (offset =="" || string==""||typeof offset!=="number" ||typeof string!=="string"){
-  //  throw new TypeError('Insira uma senha válida ou confira a mensagem digitada.')
- // }
+ 
   let resultadoCipher="";
   for (let i = 0; i<string.length; i++){
     
     let n = string.charCodeAt(i);
-   // let letraFinal = String.fromCharCode((((n-90) - offset) % 26)+90);
-   // resultadoCipher +=letraFinal;
+  
    if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
     let n = string.charCodeAt(i);
     resultadoCipher += String.fromCharCode((((n-90) - offset) % 26)+90);
   } else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
     resultadoCipher += String.fromCharCode((((n-122) - offset) % 26)+122);
   } else if (string.charCodeAt(i)>=32 && string.charCodeAt(i)<=63){
-  //(string.charCodeAt(i)===32) {
-    //resultadoCipher += String.fromCharCode(n);
+  
     resultadoCipher += String.fromCharCode(((n-32) % 31)+32);
-  //} else if (string.charCodeAt(i)===128 ){
-   // resultadoCipher += String.fromCharCode(128);
-  //} else if (string.charCodeAt(i)===135 ){
-  //  resultadoCipher += String.fromCharCode(135);
+ 
   }else {
     resultadoCipher+= String.fromCharCode(n);
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import cipher from './cipher.js';
 
 
-let cifrou=document.getElementById("encode").addEventListener("click", encode );
+document.getElementById("encode").addEventListener("click", encode );
 document.getElementById("decode").addEventListener("click", decode );
 document.getElementById("limpar").addEventListener("click", limpar );
 document.getElementById("confirmar").addEventListener("click", confirmar );
@@ -29,20 +29,21 @@ function decode(event){
   }else{document.getElementById("msgsaida").innerHTML=cipher.decode(parseInt(offset), string); 
 }
 }
-function limpar(){
-  // event.preventDefault();
+function limpar(event){
+  event.preventDefault();
    document.getElementById("offset").value="";
    document.getElementById("msgentrada").value="";
-   document.getElementById("msgsaida").value="";
+   //document.getElementById("msgsaida").value="";
    document.getElementById("pessoasCadastradas").value="";    
 }
 
 function confirmar(event){
-    event.preventDefault();
-    document.getElementById("formulario").style.display="none";
-    document.getElementById("voltar").style.display="block";
-    document.getElementById("final").style.display="block";
-    document.getElementById("confirmar").style.display="none";
+  event.preventDefault();
+  document.getElementById("formulario").style.display="none";
+  document.getElementById("voltar").style.display="block";
+  document.getElementById("final").style.display="block";
+  document.getElementById("confirmar").style.display="none";
+  document.getElementById("img").style.display="none";
   let pessoas = document.getElementById("pessoasCadastradas").value;
   let gabi = document.getElementById("1").value;
   let jardel = document.getElementById("2").value;
@@ -53,29 +54,30 @@ function confirmar(event){
  
    if (pessoas===gabi){
          
-     document.getElementById("off").innerHTML= "Sua senha é " + offset + "."
-     //let msgnova = document.getElementById("msgsaida").value;    
-      document.getElementById("suamsg").innerHTML= "Olá Gabi, voĉe recebeu uma nova mensagem. Copie sua senha e sua mensagem e volte para página inicial e para cifra-lá ou decifra-ĺá." 
-      document.getElementById("cifrada").innerHTML= "Sua mensagem é:" + msgnova;
+     document.getElementById("off").innerHTML= "Sua senha é " + offset + "."  
+     document.getElementById("suamsg").innerHTML= "Olá Gabi, voĉe recebeu uma nova mensagem. Salve sua mensagem e sua senha e volte para página inicial para cifra-lá ou decifra-ĺá." 
+     document.getElementById("cifrada").innerHTML= "Sua mensagem é:" + msgnova;
       
   }else if (pessoas==jardel){
-    document.getElementById("off").innerHTML= "Sua senha é " + offset
-    document.getElementById("suamsg").innerHTML= "Olá Jardel, voĉe recebeu uma nova mensagem. Copie sua senha e sua mensagem e volte para página inicial e para cifra-lá ou decifra-ĺá." 
-    document.getElementById("cifrada").innerHTML= "Sua mensagem é:" + msgnova;
+     document.getElementById("off").innerHTML= "Sua senha é " + offset
+     document.getElementById("suamsg").innerHTML= "Olá Jardel, voĉe recebeu uma nova mensagem. Salve sua mensagem e sua senha e volte para página inicial para cifra-lá ou decifra-ĺá."  
+     document.getElementById("cifrada").innerHTML= "Sua mensagem é:" + msgnova;
      
   }else {
-    document.getElementById("off").innerHTML= "Sua senha é " + offset
-    document.getElementById("suamsg").innerHTML= "Olá Carla, voĉe recebeu uma nova mensagem. Copie sua senha e sua mensagem e volte para página inicial e para cifra-lá ou decifra-ĺá." 
-    document.getElementById("cifrada").innerHTML= "Sua mensagem é:" + msgnova;  
+     document.getElementById("off").innerHTML= "Sua senha é " + offset
+     document.getElementById("suamsg").innerHTML= "Olá Carla, voĉe recebeu uma nova mensagem. Salve sua mensagem e sua senha e volte para página inicial para cifra-lá ou decifra-ĺá."  
+     document.getElementById("cifrada").innerHTML= "Sua mensagem é:" + msgnova;  
     }
+    
   }
   function voltar(){
     document.getElementById("formulario").style.display="block";
     document.getElementById("final").style.display="none";
     document.getElementById("offset").value="";
     document.getElementById("msgentrada").value="";
-    
+   // document.getElementById("msgsaida").value="";
     document.getElementById("pessoasCadastradas").value=""; 
+    document.getElementById("img").style.display="block";
   }
 
    console.log();
